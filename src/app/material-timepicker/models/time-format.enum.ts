1197479594997243bd0ea887648f10a40b5560d4
William Hall <mrtheme@gmail.com>
2018-08-29 13:54:25 -0500

    Update time-format.enum.ts
    fix spacing

--- src/app/material-timepicker/models/time-format.enum.ts
+++ src/app/material-timepicker/models/time-format.enum.ts
@@ -1,6 +1,6 @@
 export enum TimeFormat {
 	TWELVE = 'hh:mm a',
 	TWENTY_FOUR = 'HH:mm',
-  TWELVE_WITH_SECONDS = 'hh:mm:ss a',
-  TWENTY_FOUR_WITH_SECONDS = 'HH:mm:ss'
+    TWELVE_WITH_SECONDS = 'hh:mm:ss a',
+    TWENTY_FOUR_WITH_SECONDS = 'HH:mm:ss'
 }
